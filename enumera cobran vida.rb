a27fa76e7cd17fae974d78d47841805c69cea02e
codeamac02 <codeamac02@gmail.com>
2015-09-18 18:30:50 -0500

    se borra documentacion

--- enumera cobran vida.rb
+++ enumera cobran vida.rb
@@ -1,24 +1,21 @@
-# def print_odd_indexed_integers(array)
-# 	array.each_with_index do |item, index|
-# 		if index % 2 == 1
-# 			p item
-# 			end	
-# 	end
-# end
-# p print_odd_indexed_integers([2, 4, 6, 8, 10, 12]) 
+def print_odd_indexed_integers(array)
+	array.each_with_index do |item, index|
+		if index % 2 == 1
+			p item
+			end	
+	end
+end
+p print_odd_indexed_integers([2, 4, 6, 8, 10, 12]) 
 
-# def odd_integers(array)
-#     array.select do |impar|
-#     	impar.odd?
-#     end
-# end
-# puts odd_integers([3, 4, 7, 9, 10, 16])
-#  # == [3, 7, 9]
+def odd_integers(array)
+    array.select do |impar|
+    	impar.odd?
+    end
+end
+puts odd_integers([3, 4, 7, 9, 10, 16])
+ 
 
 def first_under(array, limit)
 	array.find {|i| i < limit}
 end
-puts first_under([13, 21, 7, 0, 11, 106], 15) 
-# == 7
-
-# (1..10).detect  {|i| i % 5 == 0 and i % 7 == 0 }   #=> nil
+puts first_under([13, 21, 7, 0, 11, 106], 10) 
